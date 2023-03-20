240e58dd6207aa43ce031834efeb08ba64efc25b
christopherjmedlin <christopherjmedlin@gracchus.christophermedlin.xyz>
2023-03-19 20:15:06 -0600

    fix coins

--- main.c
+++ main.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 const int DENOMS[7] = {100, 50, 20, 10, 5, 2, 1};
+const int COIN_DENOMS[4] = {25, 10, 5, 1};
 
-int main(int argc, char* argv[]) {
-  if (argc == 1) {
-    printf("Missing argument\n");
-    exit(0);
+int main(int argc, char** argv) {
+  int dollars = 0, coins = 0;
+
+  printf("Please enter a currency value in the form X.YY\n");
+  scanf("%d.%d", &dollars, &coins);
+  
+  if (coins > 99) {
+    printf("Error: precision for cents cannot be greater than 2");
+  }
+  
+  for (int i = 0; i < 7; i++) {
+    printf("$%d : %d\n", DENOMS[i], dollars / DENOMS[i]);
+    dollars %= DENOMS[i];
   }
 
-  int val = atoi(argv[1]);
-  printf("%s\n", argv[1]);
-
-  int change[7] = {0,0,0,0,0,0,0};
-
-  for (int i = 0; i < 7; i++) {
-    printf("$%d : %d\n", DENOMS[i], val / DENOMS[i]);
-    val %= DENOMS[i];
+  for (int i = 0; i < 4; i++) {
+    printf("¢%d : %d\n", COIN_DENOMS[i], coins / COIN_DENOMS[i]);
+    coins %= COIN_DENOMS[i];
   }
 }
